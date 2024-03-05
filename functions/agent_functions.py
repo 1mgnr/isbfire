8eb0fda224ae687a2d9644b1d39cdc0ed09da0e9
1mgnr <1mgner@gmail.com>
2024-03-05 09:56:52 +0530

    scoring improved table with human feedback

--- functions/agent_functions.py
+++ functions/agent_functions.py
@@ -22,20 +22,35 @@ def agent_1_question_maker(state):
     return state
 
 
+# def weighted_score(score_dict, weights):
+#     """
+#     Gives a weighted score based on weights provided.
+#     """
+#     weighted_scores = {k: score_dict[k]*weights[k] for k in score_dict}
+#     return sum(weighted_scores.values())
+
+
 def agent_3_scorer(state, answer):
 
     base_question = BASE_QUESTIONS[state["question_index"]]
     state["answers"][state["question_index"]] = answer  # Save the candidate's answer
     print(f"\nReceived Candidate's Answer {state['question_index'] + 1}:\n{state['answers']}{state['question_index']}\n")  # Log answer to console
-    score = orchestrator.assistant.converse(
-        prompt.SCORER(base_question), f"Given the scoring guidelines mentioned above, please assess the following answer: {state['answers'][state['question_index']]}, with respect to the question: {state['questions'][state['question_index']]} and relevance to {base_question}. Give response in a json object with score, the description, base question, question and answer")
     
-    print(f"\nAssessed Score for Question {state['question_index'] + 1}:\n{score}\n")  # Log score to console
+    weights = {"relevance": 0.4, "sincerity": 0.3, "communication_skills": 0.3}  # define your own weights
+
+    score_dict = orchestrator.assistant.converse(
+        prompt.SCORER(base_question), f"Given the scoring guidelines mentioned above, please assess the following answer: {state['answers'][state['question_index']]}, with respect to the question: {state['questions'][state['question_index']]} and relevance to {base_question}. If answer is one worder give lower points. Give response in a json object following keys: relevance, sincerity, communication_skills, score (weighted sum based on {weights}), base_question: {base_question}, answer:{state['answers'][state['question_index']]}, question:{state['questions'][state['question_index']]}, description (rationalie of scoring based in relevance, sincerity and communication skills)")
+    
+    print(f"\nAssessed Score for Question {state['question_index'] + 1}:\n{score_dict}\n")  # Log score to console
+
+    # weighted_score_total = weighted_score(score_dict, weights)
+
+    score_json = json.loads(score_dict)
 
     # Prepare document to save
-    doc_to_save = score
+    doc_to_save = score_dict
     
-    state["scores"][state["question_index"]] = score
+    state["scores"][state["question_index"]] = score_json["score"]
 
      # Increase the step
     state["step"] += 1
