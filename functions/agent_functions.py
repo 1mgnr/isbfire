9dc873f0a0879e4c1b9c5f45f02e177d7687093a
1mgnr <1mgner@gmail.com>
2024-03-05 08:26:25 +0530

    reset

--- functions/agent_functions.py
+++ functions/agent_functions.py
@@ -25,41 +25,26 @@ def agent_1_question_maker(state):
 def agent_3_scorer(state, answer):
 
     base_question = BASE_QUESTIONS[state["question_index"]]
-
     state["answers"][state["question_index"]] = answer  # Save the candidate's answer
-    
     print(f"\nReceived Candidate's Answer {state['question_index'] + 1}:\n{state['answers']}{state['question_index']}\n")  # Log answer to console
-        
-    score_string = orchestrator.assistant.converse(
-        prompt.SCORER(base_question), f"Given the scoring guidelines mentioned above, please assess the following answer: {state['answers'][state['question_index']]}, with respect to the question: {state['questions'][state['question_index']]} and relevance to {base_question}. Give response in a json object with 1. final_score (number), 2. relevance_to_question (number), 3. seriousness_of_answer (number), 4. communication_skills (number), 5. the description, 6. base question: {base_question}, 7. question and 8. answer: {state['answers'][state['question_index']]}")
+    score = orchestrator.assistant.converse(
+        prompt.SCORER(base_question), f"Given the scoring guidelines mentioned above, please assess the following answer: {state['answers'][state['question_index']]}, with respect to the question: {state['questions'][state['question_index']]} and relevance to {base_question}. Give response in a json object with score, the description, base question, question and answer")
     
-    print(f"\nAssessed Score for Question {state['question_index'] + 1}:\n{score_string}\n")  # Log score to console
+    print(f"\nAssessed Score for Question {state['question_index'] + 1}:\n{score}\n")  # Log score to console
 
-    score_dict = json.loads(score_string)  # assuming the score_string is valid JSON
-
-    # Calculate the weighted average
-    final_score = (0.5 * score_dict["relevance_to_question"] +
-                   0.3 * score_dict["seriousness_of_answer"] +
-                   0.2 * score_dict["communication_skills"])
+    # Prepare document to save
+    doc_to_save = score
     
-    if final_score > 2:
-        is_satisfactory = True
-    else:
-        is_satisfactory = False
-    
-    state["scores"][state["question_index"]] = final_score
+    state["scores"][state["question_index"]] = score
 
-    # Increase the step
+     # Increase the step
     state["step"] += 1
     state["question_index"] += 1
 
-    # Form a dictionary with individual score and final score
-    doc_to_save_dict = {"final_score": final_score, "individual_scores": score_dict, "is_satisfactory": is_satisfactory}
+    is_satisfactory=True
 
-    # Convert dictionary to string
-    doc_to_save = json.dumps(doc_to_save_dict)
-
-    return state, doc_to_save , is_satisfactory
+    # Return updated state
+    return state, doc_to_save, is_satisfactory
 
 
 def get_summary(message):
